307cc8132ee34813fecb9f604f27892daa561921
schofija <schofija@oregonstate.edu>
2021-05-31 22:01:45 -0700

    Wrote solution for test 4: After confirmed a number is divisible by both 4 and 100, the program will check if it is divisible by 400. If it is divisible by all 3, True is returned.

--- leapyear.py
+++ leapyear.py
@@ -1,7 +1,9 @@
 def check(year):
     if year % 4 == 0:
         if year % 100 == 0:
-            return 0
+            if year % 400 == 0:
+                return True
+            return False
             
         return True
 
